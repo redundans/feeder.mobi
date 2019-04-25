7a43a9003b2b378bbb323f4a4925c30c7f268950
Jesper Nilsson <jesper@klandestino.se>
2019-04-25 12:44:10 +0200

    Adds og image to theme.

--- wp-content/themes/feeder/functions.php
+++ wp-content/themes/feeder/functions.php
@@ -342,10 +342,7 @@ add_action(
 
 		if ( ! has_post_thumbnail( $post->ID ) ) { // the post does not have featured image, use a default image.
 			// Create a default image on your server or an image in your media library, and insert it's URL here.
-			$default_image = 'http://example.com/image.jpg';
-			if ( get_custom_logo_src() ) {
-				$default_image = get_custom_logo_src();
-			}
+			$default_image = get_template_directory_uri() . '/images/feeder.png';
 			echo '<meta property="og:image" content="' . esc_url( $default_image ) . '"/>';
 		} else {
 			$thumbnail_src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'medium' );
@@ -354,16 +351,3 @@ add_action(
 	},
 	10
 );
-
-/**
- * Get url for ustom logo if it exists.
- */
-function get_custom_logo_src() {
-	if ( has_custom_logo() ) {
-		$custom_logo_id = get_theme_mod( 'custom_logo' );
-		$logo_meta      = wp_get_attachment_image_src( $custom_logo_id, 'full' );
-		return $logo_meta[0];
-	} else {
-		return false;
-	}
-}
